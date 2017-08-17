b769956ea292ef5120e27a6534c133b65c439e4a
Aziz <z13z4ck@gmail.com>
2017-08-17 23:24:41 +0800

    print multiple output
    try with different virtual pin

--- blyrpi.py
+++ blyrpi.py
@@ -4,12 +4,25 @@ BLYNK_AUTH = '60c5c89c4cb243428d02140bdaf4f896'
 
 blynk = BlynkLib.Blynk(BLYNK_AUTH)
 
+pin0 = None
+pin1 = None
+
+
+@blynk.VIRTUAL_WRITE(1)
+def write_pin1(value):
+    pin1 = value
+
+
+@blynk.VIRTUAL_WRITE(0)
+def write_pin0(value):
+    pin0 = value
+
+@blynk.VIRTUAL_WRITE(5)
+def write_pin5(value):
+    print("Pin0 = ", pin0, "\tPin1 = ", pin1)
 
 
 
-@blynk.VIRTUAL_READ(1)
-def write_handle(value):
-    print('Current V1 Value: {}'.format(value))
 
 
 blynk.run()
