bfd577bb116a478ccf210fb0274eead7eadafbfb
Aziz <z13z4ck@gmail.com>
2017-08-19 15:58:18 +0800

    using local class
    not using class decorator

--- blyrpi.py
+++ blyrpi.py
@@ -35,32 +35,36 @@ import BlynkLib
 class rpi(object):
 
 
-    BLYNK_AUTH = '60c5c89c4cb243428d02140bdaf4f896'
-    blynk = BlynkLib.Blynk(BLYNK_AUTH)
+    # BLYNK_AUTH = '60c5c89c4cb243428d02140bdaf4f896'
+    # blynk = BlynkLib.Blynk(BLYNK_AUTH)
 
     def __init__(self):
-        # BLYNK_AUTH = '60c5c89c4cb243428d02140bdaf4f896'
-        # self.blynk = BlynkLib.Blynk(BLYNK_AUTH)
+        BLYNK_AUTH = '60c5c89c4cb243428d02140bdaf4f896'
+        self.blynk = BlynkLib.Blynk(BLYNK_AUTH)
 
         self.pin0 = None
         self.pin1 = None
 
+        self.blynk.add_virtual_pin(0, write=self.write_pin0())
+        self.blynk.add_virtual_pin(1, write=self.write_pin1())
+        self.blynk.add_virtual_pin(5, write=self.write_pin5())
+
         self.blynk.run()
 
-    @blynk.VIRTUAL_WRITE(1)
+    # @blynk.VIRTUAL_WRITE(1)
     def write_pin1(self, value):
         self.pin1 = value
 
-    @blynk.VIRTUAL_WRITE(0)
+    # @blynk.VIRTUAL_WRITE(0)
     def write_pin0(self, value):
         self.pin0 = value
 
-    @blynk.VIRTUAL_WRITE(5)
+    # @blynk.VIRTUAL_WRITE(5)
     def write_pin5(self, value):
         print("Pin0 = ", self.pin0, "\tPin1 = ", self.pin1)
 
-    def run(self, func=blynk):
-        func.run()
+    # def run(self, func=blynk):
+    #     func.run()
 
 
 if __name__ == "__main__":
